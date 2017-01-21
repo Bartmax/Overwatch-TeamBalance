cb0912a800a4905d7762bc45b190dcd292aa4a07
Bart <bart@bartmax.com>
2017-01-21 07:37:55 -0300

    initial release.

--- src/Overwatch/Program.cs
+++ src/Overwatch/Program.cs
@@ -14,7 +14,7 @@ namespace Overwatch
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+                .UseAzureAppServices()
                 .UseStartup<Startup>()
                 .Build();
 
